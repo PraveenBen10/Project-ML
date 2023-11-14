e95e87355caadbff29f33c24fccad369c8a8d592
PraveenBen10 <131775764+PraveenBen10@users.noreply.github.com>
2023-11-15 02:03:46 +0530

    Update app.py

--- app.py
+++ app.py
@@ -24,6 +24,9 @@ dres = {1: "YES", 0: "NO"}
 
 @app.route('/')
 def home():
+    if __name__ == '__main__':
+        port = int(os.environ.get('PORT', 5000))
+        app.run(host='0.0.0.0', port=port)
     return render_template('index.html')
 
 
