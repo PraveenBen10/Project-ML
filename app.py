f758a2594fc46dff2647c067fb1827c9640a3659
PraveenBen10 <131775764+PraveenBen10@users.noreply.github.com>
2023-11-15 02:56:59 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import numpy as np
 from flask import Flask, request, jsonify, render_template
 import pickle
-from collections import Counter
+# from collections import Counter
 import os
 
 app = Flask(__name__)
